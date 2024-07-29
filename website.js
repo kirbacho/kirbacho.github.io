2f8885b27eb03dc912e4b122aaf7d67e8f3cbebf
Bakir Colic <99488276+kirbacho@users.noreply.github.com>
2024-07-30 01:12:25 +0200

    Add files via upload

---
+++ website.js
@@ -0,0 +1,12 @@
+function sum (a,b){
+	let res = a+b;
+	return res;
+}
+document.querySelector("html").addEventListener("Click", sum);
+const heading = document.querySelector("h1");
+heading.textContent= "Hello vro";
+const link = document.querySelector("ul");
+
+function doshit (){
+	if 
+}
